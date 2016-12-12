20babb1614c4f9ec7bb55a46bff1fe8901362557
hanguyen <hanguyen@heartflow.com>
2016-12-11 23:39:52 -0800

    Provide text detection codes and necessary resources

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# text_detection
-Text detection
+# Text detection
+
+This project aims to detect textes in images using **Extremal Region Filter** algorithm.
+
+![Matching result]('./demo.jpg')
+
+## Installation OpenCV on Mac OS
+This is summarized from http://www.pyimagesearch.com/2015/06/15/install-opencv-3-0-and-python-2-7-on-osx/
+
+1. Requires Python 2.7, Git
+2. Install Python packages
+  ```
+pip install -r requirement.txt
+  ```
+  
+3. Install necesssary packages
+  ```
+brew install cmake pkg-config jpeg libpng libtiff openexr eigen tbb
+  ```
+  
+4. Clone OpenCV repos
+  * opencv
+ ```
+cd ~
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+git checkout master
+ ```
+   * opencv_contrib
+   ```
+cd ~
+git clone https://github.com/Itseez/opencv_contrib
+cd opencv_contrib
+git checkout master
+   ```
+   
+5. Build and install OpenCV
+  ```
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D PYTHON2_PACKAGES_PATH=~/feature/lib/python2.7/site-packages \
+	-D PYTHON2_LIBRARY=/usr/bin/python \
+	-D PYTHON2_INCLUDE_DIR=/System/Library/Frameworks/Python.framework/Headers \
+	-D INSTALL_C_EXAMPLES=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+	-D BUILD_EXAMPLES=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules ..
+make -j4
+make install
+  ```
+  
+6. Verify in python shell
+  ```python
+>>> import cv2
+>>> cv2.__version__
+'3.1.0-dev'
+  ```
+
+## Usage
+
+To show all available options:
+```
+python text.py -h
+```
+
+An example:
+
+```
+python feature.py  -m  <1: Detection using Extremal Region Filter, 2: OCR Recognition using Tesseract> \
+		           -i <path to input image> \
+		           -o <path to output image>
+```
---
+++ src/text.py
@@ -0,0 +1,147 @@
+"""
+Scene Text Detection and Recognition
+Licence: BSD
+Author : Hoang Anh Nguyen
+"""
+
+import cv2
+import numpy as np
+import argparse, sys
+
+#--------------------------------------------------------
+#--------------------------------------------------------
+# Class provide an interface to detect texts in images
+class TextDetector:
+    
+    def __init__(self, image_name):
+        # read image file and convert it to grayscale
+        self.origin_image = cv2.imread(image_name)
+        self.gray_image   = cv2.cvtColor(self.origin_image,
+                                         cv2.COLOR_BGR2GRAY)
+        
+        self.ER_GROUPING_ORIENTATION = 0#cv2.text.ERGROUPING_ORIENTATION_ANY
+        
+        # load pre-trained ER classifiers
+        fileERClassifier1 = cv2.text.loadClassifierNM1(
+            './data/trained_classifierNM1.xml')
+        self.ERClassifier1 = cv2.text.createERFilterNM1(fileERClassifier1,
+                                                        16, 0.00015, 0.13,
+                                                        0.2, True, 0.1)
+        fileERClassifier2 = cv2.text.loadClassifierNM2(
+            './data/trained_classifierNM2.xml')
+        self.ERClassifier2 = cv2.text.createERFilterNM2(fileERClassifier2,
+                                                        0.5)
+        
+        # store all found regions
+        self.regions = []
+        
+    #--------------------------------------------------------
+    # Main methods
+    #--------------------------------------------------------
+    
+    # Extract features in the image using a specific Detector/Descriptor
+    # input:    showResult
+    # return:   number of keypoints
+    #           description of each keypoint
+    def extract(self, showResult = False):
+        
+        # Extract channels to be processed individually
+        channels = cv2.text.computeNMChannels(self.origin_image)
+        # Append negative channels to detect ER- (bright regions over dark background)
+        cn = len(channels) - 1
+        for c in range(0, cn):
+            channels.append((255 - channels[c]))
+
+        # Apply the default cascade classifier to each independent channel
+        # (could be done in parallel)
+        print("Extracting Class Specific Extremal Regions from "
+              + str(len(channels)) + " channels ...")
+        for channel in channels:
+            regions = cv2.text.detectRegions(channel, 
+                                             self.ERClassifier1,
+                                             self.ERClassifier2)
+
+            if self.ER_GROUPING_ORIENTATION == cv2.text.ERGROUPING_ORIENTATION_ANY:
+                rects = cv2.text.erGrouping(self.origin_image,
+                                            channel,
+                                            [r.tolist() for r in regions],
+                                            cv2.text.ERGROUPING_ORIENTATION_ANY,
+                                            './data/trained_classifier_erGrouping.xml',
+                                            0.5)
+            else:
+                rects = cv2.text.erGrouping(self.origin_image,
+                                        channel,
+                                        [r.tolist() for r in regions])
+            # save regions
+            [self.regions.append(rect) for rect in rects]
+
+        # Visualization
+        vis = self.origin_image.copy()
+        for r in range(0, np.shape(self.regions)[0]):
+            rect = self.regions[r]
+            cv2.rectangle(vis,
+                          (rect[0],rect[1]),
+                          (rect[0]+rect[2],
+                           rect[1]+rect[3]),
+                          (0, 0, 0), 2)
+            cv2.rectangle(vis,
+                          (rect[0],rect[1]),
+                          (rect[0]+rect[2],
+                           rect[1]+rect[3]),
+                          (255, 255, 255), 1)
+        
+        return vis
+#            cv2.imshow("Text detection result", vis)
+#            cv2.waitKey(0)
+    
+    # draw keypoints & descriptors on origin image
+    def showFeatures(self):
+        if not self.detector.keypoints:
+            print('Keypoint not found!')
+            return None
+        
+        cv2.drawKeypoints(self.origin_image,
+                          self.detector.keypoints,
+                          self.origin_image,
+                          (0, 255, 0))
+        return self.origin_image
+        
+#--------------------------------------------------------
+#--------------------------------------------------------
+def main(argv):
+    # Define argument list. Example:
+    # python text.py -m 1
+    #                -i test/scenetext_segmented_word04.jpg
+    #                -o .
+    parser = argparse.ArgumentParser(description='Scene Text Detection and Recognition')
+    parser.add_argument('-m','--method',
+                        help="""Specify method:
+                        1: Detection using Extremal Region Filter,
+                        2: OCR Recognition using Tesseract
+                        """,
+                        required=True)
+    parser.add_argument('-i','--input',
+                        help='Input image',
+                        required=True)
+    parser.add_argument('-o','--output',
+                        help='Ouput location',
+                        required=True)
+    args = vars(parser.parse_args())
+    
+    # extract arguments
+    method = int(args['method'])
+    
+    if method != 1 and method != 2:
+        print("Invalid method: " + args['method'])
+        return
+    
+    # find keypoints & build descriptor on input1 image
+    detector = TextDetector(args['input'])
+    output = detector.extract(method == 1)
+    
+    # save output
+    cv2.imwrite(args['output'] + '/output.jpg', output)
+    print("Output saved!")
+    
+if __name__ == '__main__':
+    main(sys.argv)
